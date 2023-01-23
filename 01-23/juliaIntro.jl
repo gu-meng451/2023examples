dc95fbf3c0ea0dfbf263a1b50ed631ff9c9477a1
tim <fitzgeraldt@gonzaga.edu>
2023-01-23 12:31:42 -0800

    Updates from class

--- 01-23/juliaIntro.jl
+++ 01-23/juliaIntro.jl
@@ -1,15 +1,48 @@
 ## Packages and enviroments
+
 using Pkg
 Pkg.activate(".")
+# Pkg.add("Polynomials")
+
+# "]" in the REPL puts us in Pkg mode
 
 ## Types
 # Numbers: Integers, Floats, Complex
 
+a = 1
+b = 1.
+
+c::Int16 = 2
+
+d = 1 + 3.0im
+typeof(d)
+
 # Arrays: 1D, 2D, Floats, Any
+A = [ 1 2 3 4]
+B = [1; 2; 3; 4.]
+D = [1+2 (3 + 4)]
+
+E = [1 3;
+    2 5]
+
+E[1,:]
+E[:,2]
+a = 1
+
+# Tuple (cannot easily change after making):
+q = (1,2,B)
+
+a,b,c2 = q
 
 # Strings
+`a = "this is a string"`
 
 # Dictionaries
+A = Dict(1 => "value1", "2" => rand() )
+# so to access it we use the 'key'
+A[1]
+A["2"]
+A[:newsym] = 4
 
 # Strucs
 mutable struct myCustomType

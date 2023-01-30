216c73e1433a298420bbff3940e306d876ca682f
tim <fitzgeraldt@gonzaga.edu>
2023-01-30 13:20:52 -0800

    live from class

--- 01-30/expdemo.jl
+++ 01-30/expdemo.jl
@@ -6,20 +6,45 @@ using Printf
  A series based calculation of exp(x) to an optionally specifiable tolerance.
 
 """
-function  myExp(x; tol=1e-14, maxIter=300, verbose=false)
+function myExp(n::Int)
+    e = 2.71828182845904523536028747135266249775724709369995
+
+    y = 1.0
+
+    for i in 1:n
+        y *= e
+    end
+
+    return y
+
+end
+
+function  myExp(X; tol=1e-14, maxIter=300, verbose=false)
 
 convergenceFlag = false
 
 # initialize the output variable
-y = one(typeof(x))
-newterm = one(typeof(x))
+y = one(typeof(X))
+newterm = one(typeof(X))
+
+## if x < 0 flip it
+x = X
+if X < 0
+    x = -X
+end
+
+## break problem into part
+w = floor(Int, x)
+z = x - w
+
+expW = myExp(w)
 
 iter = 0
 
 while !convergenceFlag
     iter += 1
 
-    newterm *= x/iter
+    newterm *= z
     y += newterm
 
     rel_error = abs( newterm )
@@ -29,7 +54,13 @@ while !convergenceFlag
 
     if rel_error <= tol
         convergenceFlag = true
-        return y
+
+        if X < 0
+            return 1/(y*expW)
+        else
+            return y*expW
+        end
+
     elseif iter >= maxIter
         error("Solution failed to converge in $iter steps")
     end
@@ -40,6 +71,6 @@ end
 
 myExp(1.) - exp(1.)
 
-myExp(-100., verbose=true)
+myExp(-100.5, verbose=true)
 
 # what happens when x is very large or very small?

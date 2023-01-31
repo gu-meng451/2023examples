1a88289c9a55263811031d6caf5479d4085d947e
tim <fitzgeraldt@gonzaga.edu>
2023-01-31 08:28:12 -0800

    tweaks to the sparse example

--- 02-01/sparse.jl
+++ 02-01/sparse.jl
@@ -1,22 +1,33 @@
+using Pkg
+Pkg.activate(".")
 using SparseArrays
 using Plots
 using JLD2
 
-vals = [1., 2., 3., 4., 5., 0.]
-idx_i= [ 1, 2, 4, 6, 7, 7]
-idx_j= [ 1, 2, 4, 6, 1, 7]
+vals = [1.0, 2.0, 3.0, 4.0, 5.0, 0.0]
+idx_i = [1, 2, 4, 6, 7, 7]
+idx_j = [1, 2, 4, 6, 1, 7]
 
 A = sparse(idx_i, idx_j, vals)
 
 spy(A, markersize=10)
-
 
 ## load up an FEM example stiffness matrix
 jldopen("sparsedemo.h5", "r") do f
     i = f["/i"]
     j = f["/j"]
     v = f["/v"]
-    global K = sparse(i,j,v)
+    global K = sparse(i, j, v)
 end
 
 spy(K)
+
+## Reordering is a thing
+using AMD
+# Extract the free-free part of the stiffness matrix:
+n = 1500
+k1 = K[1:n, 1:n]
+# pick a reording: amd, symamd, colamd
+p = amd(k1)
+k2 = k1[p, p]
+spy(k2)

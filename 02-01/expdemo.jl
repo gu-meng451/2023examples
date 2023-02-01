e90980589a0c2bdfa3df1d00b05f348f2bdebba0
tim <fitzgeraldt@gonzaga.edu>
2023-02-01 08:06:44 -0800

    add built-in comparison

--- 02-01/expdemo.jl
+++ 02-01/expdemo.jl
@@ -21,63 +21,64 @@ function myExp(n::Int)
 
 end
 
-function  myExp(X; tol=1e-14, maxIter=300, verbose=false)
+function myExp(X; tol=1e-14, maxIter=300, verbose=false)
 
-convergenceFlag = false
+    convergenceFlag = false
 
-# initialize the output variable
-y = one(typeof(X))
-newterm = one(typeof(X))
+    # initialize the output variable
+    y = one(typeof(X))
+    newterm = one(typeof(X))
 
-## if x < 0 flip it
-x = X
-if X < 0
-    x = -X
-end
-
-## break problem into part
-w = floor(Int, x)
-z = x - w
-
-expW = myExp(w)
-
-iter = 0
-
-while !convergenceFlag
-    iter += 1
-
-    newterm *= z/iter
-    y += newterm
-
-    rel_error = abs( newterm )
-    if verbose
-        @printf("iter = %3s, rel error = %.3e\n",iter,rel_error)
+    ## if x < 0 flip it
+    x = X
+    if X < 0
+        x = -X
     end
 
-    if rel_error <= tol
-        convergenceFlag = true
+    ## break problem into part
+    w = floor(Int, x)
+    z = x - w
 
-        if X < 0
-            return 1/(y*expW)
-        else
-            return y*expW
+    expW = myExp(w)
+
+    iter = 0
+
+    while !convergenceFlag
+        iter += 1
+
+        newterm *= z / iter
+        y += newterm
+
+        rel_error = abs(newterm)
+        if verbose
+            @printf("iter = %3s, rel error = %.3e\n", iter, rel_error)
         end
 
-    elseif iter >= maxIter
-        error("Solution failed to converge in $iter steps")
-    end
+        if rel_error <= tol
+            convergenceFlag = true
 
-end
+            if X < 0
+                return 1 / (y * expW)
+            else
+                return y * expW
+            end
+
+        elseif iter >= maxIter
+            error("Solution failed to converge in $iter steps")
+        end
+
+    end
 
 end
 
 ## Simple tests
-myExp(1.) - exp(1.)
+myExp(1.0) - exp(1.0)
 
 myExp(-100.5, verbose=true)
 
 ## Better tests
 using BenchmarkTools
 
-@benchmark myExp(x) setup=(x=100*(2*rand()-1))
+@benchmark myExp(x) setup = (x = 100 * (2 * rand() - 1))
 
+@benchmark exp(x) setup = (x = 100 * (2 * rand() - 1))

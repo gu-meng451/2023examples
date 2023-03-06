df2dadbad5c44525195fd21c20edc937f7a370c2
tim <fitzgeraldt@gonzaga.edu>
2023-03-06 12:10:55 -0800

    add some annotations to the plot

--- 03-06/main.jl
+++ 03-06/main.jl
@@ -18,10 +18,14 @@ end
 L(x,X,Y) = sum( [ lag(x, j, X)*Y[j] for j in 1:length(Y) ] )
 
 X = [-1, 2, 4, 7]
-Y = sin.(X)
+f(x) = sin(x)
+Y = f.(X)
 
-x_range = LinRange(-1,7,10)
-y_interp = [ L(x,X, Y) for x in x_range]
+x_range = LinRange(-1,7,40)
+y_interp = [ L( x, X, Y) for x in x_range ]
 
-plot(x_range, y_interp)
-scatter!(X, Y)
+plot( f, -1, 7, lw=2, label="sin(x)" )
+plot!(x_range, y_interp, 
+    lw=2, line_style=:dash, 
+    label="Interpolation")
+scatter!(X, Y, marker_size=4, label="Control Points")

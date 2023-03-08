67b12dd9ac7b30dce4008bcabc4d30e83ad25440
tim <fitzgeraldt@gonzaga.edu>
2023-03-08 08:14:13 -0800

    updated

--- 03-08/main.jl
+++ 03-08/main.jl
@@ -14,7 +14,8 @@ L(0.1)
 S = Spline.spinterp(X,Y)
 S(0.1)
 
-plot(f, -1, 7)
-plot!(L, -1, 7)
-plot!(S, -1, 7)
-scatter!(X,Y, marker=3)
+scatter(X,Y, marker=3, label="control points", 
+    xlabel="x", ylabel="y(x)")
+plot!(L, -1, 7, label="Lagrange")
+plot!(S, -1, 7, label="Spline")
+plot!(f, -1, 7, label="Original function")

ae9127a7d6db97f5fc3c71c96b47c1b5c26b8d81
tim <fitzgeraldt@gonzaga.edu>
2023-04-26 07:45:38 -0700

    prep for class today

---
+++ fem/HeatTransfer2D.jl
@@ -0,0 +1,5 @@
+module HeatTransfer2D
+
+
+
+end
--- fem/Shapefunctions.jl
+++ fem/Shapefunctions.jl
@@ -15,24 +15,59 @@ function line(r::AbstractFloat)
 end
 line(r) = line(r[1])
 
-function quad(r,s)
+"""
+    triangles(r,s)
 
-    NN = NaN
-    Nr = NaN
-    Ns = NaN
+Shape functions for a 3 node triangle
+`` r \\in [-1,1] ``, `` s \\in [-1,1] ``, `` r + s \\leq 0 ``
+"""
+function triangle(r, s)
+    NN = [(1 / 2) * ((-1) * r + (-1) * s), (1 / 2) * (1 + r), (1 / 2) * (1 + s)]
+    # derivatives
+    Nr = [(-1 / 2), (1 / 2), 0]
+    Ns = [(-1 / 2), 0, (1 / 2)]
     return NN, Nr, Ns
-
 end
-quad(r) = quad(r[1],r[2])
+triangle(x) = triangle(x[1], x[2])
+
+"""
+    quads(r,s)
+
+Shape functions for a 4 node quadrilateral
+`` r \\in [-1,1] ``, `` s \\in [-1,1] ``
+"""
+function quad(r, s)
+    NN = [(1 / 4) * ((-1) + r) * ((-1) + s), (-1 / 4) * (1 + r) * ((-1) + s),
+        (1 / 4) * (1 + r) * (1 + s), (-1 / 4) * ((-1) + r) * (1 + s)]
+    # derivatives
+    Nr = [
+        (1 / 4) * ((-1) + s),
+        (1 / 4) * (1 + (-1) * s),
+        (1 / 4) * (1 + s),
+        (1 / 4) * ((-1) + (-1) * s),
+    ]
+    Ns = [
+        (1 / 4) * ((-1) + r),
+        (1 / 4) * ((-1) + (-1) * r),
+        (1 / 4) * (1 + r),
+        (1 / 4) * (1 + (-1) * r),
+    ]
+    return NN, Nr, Ns
+end
+quad(x) = quad(x[1], x[2])
 
 function shapefunc(el_type::String)
     if el_type == "line"
         return line
+    elseif el_type == "triangle"
+        return triangle
     elseif el_type == "quad"
         return quad
     else
         error("Element type not found")
     end
+end
+
 end
 
 end
